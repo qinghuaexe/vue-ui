393861e2830ab52f89d6d62648316a7b65e6a686
qinghuaexe <qinghuaexe@gmail.com>
2019-03-28 18:37:33 +0800

    添加按钮位置调整

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,19 @@
 <template>
-    <button class="g-button">按钮</button>  
+  <div>
+    <button class="g-button" :class="`icon-${iconPosition}`">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href="`#icon-${icon}`"></use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
+  </div>
 </template>
 <script>
-export default {}
+export default {
+  props: ["icon", "iconPosition"]
+}
 </script>
 <style lang="scss">
 .g-button {
@@ -12,6 +23,9 @@ export default {}
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: flex;
+  justify-content: center;
+  align-items: center;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -20,6 +34,24 @@ export default {}
   }
   &:focus {
     outline: none;
+  }
+  >.content {
+    order: 2;
+  }
+  >.icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+
+  &.icon-right {
+    >.content {
+      order: 1;
+    }
+    >.icon {
+      order: 2;
+      margin-left: 0.1em;
+      margin-right: 0;
+    }
   }
 }
 </style>
