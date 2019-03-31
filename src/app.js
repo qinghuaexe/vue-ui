e06f6d3db0350839d62c1162d29b612916a55cd0
qinghuaexe <qinghuaexe@gmail.com>
2019-03-31 14:30:04 +0800

    调整loading位置绑定click时间

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
-import Vue from 'vue'
-import Button from './button.vue'
-import Icon from './icon.vue'
+import Vue from "vue"
+import Button from "./button.vue"
+import Icon from "./icon.vue"
 
-Vue.component('g-icon', Icon)
-Vue.component('g-button', Button)
+Vue.component("g-icon", Icon)
+Vue.component("g-button", Button)
 
 new Vue({
-        el: "#app"
-      })
+  el: "#app",
+  data: {
+    loading1: false,
+    loading2: true,
+    loading3: false
+  }
+});
