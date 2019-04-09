bd71406e8f52fcac63aaceacd2558a5bd7e5ac47
qinghuaexe <qinghuaexe@gmail.com>
2019-04-09 19:19:22 +0800

    添加disable组件选中功能

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -13917,7 +13917,8 @@ var _default = {
   computed: {
     classes: function classes() {
       return {
-        active: this.active
+        active: this.active,
+        disabled: this.disabled
       };
     }
   },
@@ -13929,7 +13930,11 @@ var _default = {
     });
   },
   methods: {
-    xxx: function xxx() {
+    onClick: function onClick() {
+      if (this.disabled) {
+        return;
+      }
+
       this.eventBus.$emit("update:selected", this.name, this);
     }
   }
@@ -13949,7 +13954,11 @@ exports.default = _default;
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "tabs-item", class: _vm.classes, on: { click: _vm.xxx } },
+    {
+      staticClass: "tabs-item",
+      class: _vm.classes,
+      on: { click: _vm.onClick }
+    },
     [_vm._t("default")],
     2
   )
